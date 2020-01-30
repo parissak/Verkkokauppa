eb1063b2dc20a7d0ff087a4d678b320b22f206bd
parissak <sakari.parisaari@helsinki.fi>
2020-01-30 18:49:08 +0200

    init muokkaus

--- application/__init__.py
+++ application/__init__.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 # tietokanta
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///products.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///products.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 # db-olio
 db = SQLAlchemy(app)
@@ -35,5 +41,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-# luodaan tarvittavat tietokantataulut
-db.create_all()
+try: 
+    db.create_all()
+except:
+    pass
