eab91d57bdb3e49633947d3578b6ae8ff9a0e2e7
parissak <sakari.parisaari@helsinki.fi>
2020-01-30 18:34:13 +0200

    bugin metsästys

--- application/products/views.py
+++ application/products/views.py
@@ -1,5 +1,5 @@
 from flask import redirect, render_template, request, url_for
-from flask_login import login_required, current_user
+from flask_login import current_user
 
 from application import app, db
 from application.products.models import Product
